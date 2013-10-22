9eb43d7e4448f820fac02c15530e9aa78b239b5d
Daniel Ault <dault@memphis.edu>
2013-10-22 11:56:41 -0500

    moved pi calculation code to dboard()

--- pi.c
+++ pi.c
@@ -14,12 +14,21 @@ float rand_float();
 int main(int argc, char *argv[])
 {
 	int i, npoints = 10000;
-	int circle_count = 0;
+	
+	for (i=0; i<20; i++)
+		dboard(npoints);
+		
+	return 0;
+}
+
+int dboard(int throws)
+{
+	int i, circle_count = 0;
 	
 	int seed = time(NULL);
 	srand(seed);
 	
-	for (i=0; i<npoints; i++)
+	for (i=0; i<throws; i++)
 	{
 		float x = rand_float();
 		float y = rand_float();
@@ -27,13 +36,13 @@ int main(int argc, char *argv[])
 			circle_count = circle_count + 1;
 	}
 	
-	double pi = 4.0*circle_count/npoints;
+	double pi = 4.0*(float)circle_count/(float)throws;
 	
 	printf("circle_count is %d.\n", circle_count);
 	//printf("Pi is %d.\n", pi);
 	
+	return 1;
 }
-
 
 float rand_float()
 {
